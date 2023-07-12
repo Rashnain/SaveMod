eb936877b1e309014619878a26eebb8f32db8332
Rashnain <51191602+Rashnain@users.noreply.github.com>
2023-07-12 09:09:39 +0200

    add a key bind to open the saves list

--- src/main/java/net/rashnain/savemod/SaveMod.java
+++ src/main/java/net/rashnain/savemod/SaveMod.java
@@ -1,6 +1,12 @@
 package net.rashnain.savemod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.rashnain.savemod.gui.SelectSaveScreen;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +19,13 @@ public class SaveMod implements ClientModInitializer {
 	public static String backupName;
 
 	@Override
-	public void onInitializeClient() {}
+	public void onInitializeClient() {
+		KeyBinding openList = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.savemod.open_list", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.savemod"));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (openList.isPressed())
+				client.setScreen(new SelectSaveScreen(null));
+		});
+	}
 
 }
