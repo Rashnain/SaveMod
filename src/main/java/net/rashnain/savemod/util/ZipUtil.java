eaedab0f0393a25c0c52de3223c27fb2a48bbc2a
Rashnain <51191602+Rashnain@users.noreply.github.com>
2023-07-15 22:56:08 +0200

    a bit of refactoring

--- src/main/java/net/rashnain/savemod/util/ZipUtil.java
+++ src/main/java/net/rashnain/savemod/util/ZipUtil.java
@@ -13,27 +13,36 @@ import java.util.zip.ZipFile;
 
 public class ZipUtil {
 
-    public static void unzipFile(String zipFile, String targetDir) throws IOException {
+    public static void unzipFile(String sourceFile, String targetDir) throws IOException {
         byte[] buffer = new byte[65536];
         int length;
+
         File previous = null;
         File current;
-        try (ZipFile zip = new ZipFile(zipFile)) {
-            Enumeration<? extends ZipEntry> entries = zip.entries();
+
+        try (ZipFile archive = new ZipFile(sourceFile)) {
+
+            Enumeration<? extends ZipEntry> entries = archive.entries();
+
             while (entries.hasMoreElements()) {
+
                 ZipEntry entry = entries.nextElement();
+
                 File file = new File(targetDir, entry.getName());
+
                 current = file.getParentFile();
                 if (!current.equals(previous)) {
                     if (!current.exists())
                         current.mkdirs();
                     previous = current;
                 }
-                InputStream in = new BufferedInputStream(zip.getInputStream(entry), 65536);
+
+                InputStream in = new BufferedInputStream(archive.getInputStream(entry), 65536);
                 OutputStream out = new BufferedOutputStream(new FileOutputStream(file), 65536);
-                while ((length = in.read(buffer)) != -1) {
+
+                while ((length = in.read(buffer)) != -1)
                     out.write(buffer, 0, length);
-                }
+
                 out.close();
                 in.close();
             }
