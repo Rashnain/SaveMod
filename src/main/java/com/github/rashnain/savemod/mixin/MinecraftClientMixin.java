079358d3fba4b8a2c72aebf714bcfeff23f38c68
Rashnain <51191602+Rashnain@users.noreply.github.com>
2026-03-29 15:10:49 +0200

    fix world being "null" when saving

---
+++ src/main/java/com/github/rashnain/savemod/mixin/MinecraftClientMixin.java
@@ -0,0 +1,21 @@
+package com.github.rashnain.savemod.mixin;
+
+import com.github.rashnain.savemod.SaveMod;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.server.SaveLoader;
+import net.minecraft.world.level.storage.LevelStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+
+    @Inject(method = "startIntegratedServer", at = @At(value = "HEAD"))
+    public void startIntegratedServer(LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, boolean newWorld, CallbackInfo ci) {
+        SaveMod.worldDir = session.getDirectoryName();
+    }
+
+}
