178703d809698a2462dfe2759b3cf37ace5b0701
Rashnain <51191602+Rashnain@users.noreply.github.com>
2026-04-15 18:07:11 +0200

    keep search after resizing

--- src/main/java/com/github/rashnain/savemod/gui/SelectSaveScreen.java
+++ src/main/java/com/github/rashnain/savemod/gui/SelectSaveScreen.java
@@ -63,7 +63,7 @@ public class SelectSaveScreen extends Screen {
 
     @Override
     protected void init() {
-        searchBox = new TextFieldWidget(textRenderer, width / 2 - 100, 22, 200, 20, null, Text.empty());
+        searchBox = new TextFieldWidget(textRenderer, width / 2 - 100, 22, 200, 20, searchBox, Text.empty());
         searchBox.setChangedListener(search -> {
             saveList.setSearch(search);
             changeButtons(saveList.getSelectedOrNull() != null);
@@ -71,6 +71,7 @@ public class SelectSaveScreen extends Screen {
         addDrawableChild(searchBox);
 
         saveList = new SaveListWidget(this, client, width, height - 48 - 64, 48, 36);
+        saveList.setSearch(searchBox.getText());
         addSelectableChild(saveList);
 
         loadButton = addDrawableChild(ButtonWidget.builder(Text.translatable("savemod.list.play"), button ->
